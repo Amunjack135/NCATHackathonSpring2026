5879869642a71054ca4583eec27c2e9cd5907194
nanorparticles <alexnanor0@gmail.com>
2026-03-13 21:36:01 -0400

    Tiny bug fixes

--- Backend/Simulation.py
+++ Backend/Simulation.py
@@ -71,7 +71,7 @@ class MyOilPump:
 		if 0 <= self.__error_ratio__ <= MAX_ERROR_TICK:
 			self.__error_ratio__ += 1
 
-		error_multiplier: float = (2.5 * min(0., (self.__error_ratio__ / MAX_ERROR_TICK))) if self.__error_ratio__ > 0 else 1
+		error_multiplier: float = (2.5 * min(1., (self.__error_ratio__ / MAX_ERROR_TICK))) if self.__error_ratio__ > 0 else 1
 		target_temperature: float = (85 if self.is_running else BASE_TEMPERATURE) + (random.random() - 0.5) * 25.7 * error_multiplier
 		target_pressure: float = (205.4 if self.is_running else 100) + (random.random() - 0.5) * 58.2 * error_multiplier
 		target_flow_rate: float = (11.2 if self.is_running else 0) + (random.random() - 0.5) * 5.8 * error_multiplier
@@ -79,12 +79,12 @@ class MyOilPump:
 		target_load_percent: float = (0.95 if self.is_running else 0) + (random.random() - 0.5) * error_multiplier
 		target_vibration: float = (2.95 if self.is_running else 0) + (random.random() - 0.5) * 1.45 * error_multiplier
 
-		self.__temperature__ = (target_temperature - self.__temperature__) * TEMPERATURE_SCALAR
-		self.__pressure__ = (target_pressure - self.__pressure__) * PRESSURE_SCALAR
-		self.__flow_rate__ = (target_flow_rate - self.__flow_rate__) * FLOW_RATE_SCALAR
-		self.__rpm__ = (target_rpm - self.__rpm__) * RPM_SCALAR
-		self.__load_percent__ = (target_load_percent - self.__load_percent__) * LOAD_PERCENT_SCALAR
-		self.__vibration__ = (target_vibration - self.__vibration__) * VIBRATION_SCALAR
+		self.__temperature__ += (target_temperature - self.__temperature__) * TEMPERATURE_SCALAR
+		self.__pressure__ += (target_pressure - self.__pressure__) * PRESSURE_SCALAR
+		self.__flow_rate__ += (target_flow_rate - self.__flow_rate__) * FLOW_RATE_SCALAR
+		self.__rpm__ += (target_rpm - self.__rpm__) * RPM_SCALAR
+		self.__load_percent__ += (target_load_percent - self.__load_percent__) * LOAD_PERCENT_SCALAR
+		self.__vibration__ += (target_vibration - self.__vibration__) * VIBRATION_SCALAR
 
 		self.__operational_hours__ += time_delta_seconds
 
