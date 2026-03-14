9d5b36f8d6989b2d0585c766df1a2540b542a25f
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2026-03-13 22:58:50 -0400

    Fixed devnull pipe

--- Backend/DevNull.py
+++ Backend/DevNull.py
@@ -4,13 +4,5 @@ import CustomMethodsVI.Stream as Stream
 
 
 class NullStream(Stream.Stream):
-	__SINGLETON: NullStream = ...
-
-	def __new__(cls, *args, **kwargs) -> NullStream:
-		if NullStream.__SINGLETON is ...:
-			NullStream.__SINGLETON = super().__new__(*args, **kwargs)
-
-		return NullStream.__SINGLETON
-
 	def write(self, buffer) -> None:
 		pass
